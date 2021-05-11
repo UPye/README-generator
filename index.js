c33fb5e52298219e3776d2f6dba0b99758061b29
UPye <Urika.Pye@gmail.com>
2021-05-11 00:56:13 -0400

    Initial Commit. Starter code cloned.

---
+++ index.js
@@ -0,0 +1,24 @@
+// Task: Download NPM packages this project requires
+
+/*  Node.js & Inquirer  */
+
+// Task: Construct questions for user input ( Outline for the README )
+
+const questions = [];
+
+
+// Task: Construct the function that will write the README file
+
+function writeToFile( fileName, data ) {
+
+}
+
+// Task: Construct the function that will initialize the app
+
+function init() {
+
+}
+
+// Task: Call that function
+
+init();
