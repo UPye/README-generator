fd6b7e84e8fef92bf088245d8859caed61d47b49
UPye <Urika.Pye@gmail.com>
2021-05-14 04:27:56 -0400

    Attempting to resolve screenshot error again.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 // Task: Create a function to generate markdown for README
 const writeFile = fileContent => {
   return new Promise((resolve, reject) => {
-    fs.writeFile('./README.md', fileContent, err => {
+    fs.writeFile('./imgs/README.md', fileContent, err => {
       if (err) {
         reject(err);
         return;
